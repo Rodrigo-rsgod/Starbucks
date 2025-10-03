fef7c39c6bc03e82dcb16240315cd04ff62a96ee
Rodrigo-rsgod <deus.rodrigo@gmail.com>
2025-10-03 16:00:30 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,40 @@
+/*
+Java Script Não é Java
+Variaveis (Qualquer linguagem de Progamação)
+    - Um pedacinho de memoria do computador que 
+    separa para guardar O QUE VOCE QUISER
+
+    console.log() - ferramenta do JS tudo que eu colocar aqui dentro
+    ele coloca na tela
+    
+    Função é um pedacinho de codigo que so executa quando é chamado
+    Algoritimo - Receita 
+*/
+// let numero = 38945
+// let nome = "Rodolfo"
+// let sobrenome = "Mori"
+
+// console.log(numero) 
+// console.log(sobrenomenome)
+// console.log(sobrenome)
+
+// function digaOla (){
+//     console.log("ola")function trocaImagem(){
+// imagem.src =("img/img_01.jpg")
+
+// HTML= Document 
+// QuerySelector = selecionar o que eu qusier
+
+// }
+
+
+
+// digaOla ()
+
+
+let imagem = document.querySelector(".starbucks")
+
+function trocaImagem(endereco) {
+    imagem.src = (endereco);
+}
+
